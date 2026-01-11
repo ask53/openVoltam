4e047a5040cd04a2744c1135b0155a85b6bf6b3a
ask53 <askrupp@gmail.com>
2026-01-11 15:56:13 -0500

    remove comments from read.py

--- processes/read.py
+++ processes/read.py
@@ -16,7 +16,7 @@ def write_error(s):     # Write error to error channel
     sys.stderr.write(s)
     sys.stderr.flush()
 
-write_data(str(os.getcwd()))
+#write_data(str(os.getcwd()))
 
 
 from external.globals import ov_globals as g
@@ -26,9 +26,9 @@ from external.globals.ov_functions import get_data_from_file
     
 try:
     path = sys.argv[1]                  #   Get path of file to read from
-    write_data(path)
+    #write_data(path)
     data = get_data_from_file(path)     #   Read file from path (returns dict)
-    write_data(str(data[g.S_NAME]))
+    #write_data(str(data[g.S_NAME]))
     for run in data[g.S_RUNS]:          #   For each run in data dict
         for rep in run[g.R_REPLICATES]: #   And for each rep of the run
             rep.pop(g.R_DATA, None)     #   Remove the raw data
